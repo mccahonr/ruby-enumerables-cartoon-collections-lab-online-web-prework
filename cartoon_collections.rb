175bfda039caf603638e1d13bfb20bacba2b6c39
Ryan McCahon <mccahonr@yahoo.com>
2019-08-08 16:34:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
+  i = 0
+  while i < array.length
     puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
